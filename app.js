c927966983c8d50ed3a77fb588b3ca96f498f87d
Brian Peppard <BJPeppard@gmail.com>
2015-08-07 15:31:32 -0500

    adds flapper-news express template

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ angular.module('flapperNews', ['ui.router'])
 .controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
   $scope.test = 'Hello world!';
   $scope.posts = posts.posts;
-  // $scope.posts = [
-  //   {title: 'post 1', upvotes: 5},
-  //   {title: 'post 2', upvotes: 2},
-  //   {title: 'post 3', upvotes: 15},
-  //   {title: 'post 4', upvotes: 9},
-  //   {title: 'post 5', upvotes: 4},
-  // ];
 
   $scope.addPost = function() {
     if (!$scope.title || $scope.title === '') {
