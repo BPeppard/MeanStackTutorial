3cdfa24ce4d6a46174b34e07d0a2e582f8995373
Brian Peppard <BJPeppard@gmail.com>
2015-08-07 11:33:15 -0500

    adds form for creating posts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
+
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -11,7 +12,21 @@ function($scope){
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4},
   ];
+
   $scope.addPost = function() {
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if (!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
   };
 }]);
